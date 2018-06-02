44926a25de5c525a5b9d844907c946156467534a
Christian Spiro <christianspiro1@gmail.com>
2018-06-02 12:31:14 -0400

    Added data tags to HTML, beginning work on JS. CSS is working but not finalized.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function calculate() {
+    const screen = document.querySelector(".screen");
+    const buttons = document.getElementsByClassName("btn-light btn-dark").onclick;
+    const numPress = (buttons) => {
+
+    }
+    return buttons;
+}
+calculate(); 
